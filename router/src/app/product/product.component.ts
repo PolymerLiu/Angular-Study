7eba5e16de8269b8475d06c672cb94523edbe10f
JeffreyLiuBit <17771770679@163.com>
2019-06-16 11:44:02 +0800

    查询参数中传递数据OK

--- router/src/app/product/product.component.ts
+++ router/src/app/product/product.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {ActivatedRoute} from "@angular/router";
 
 @Component({
   selector: 'app-product',
@@ -7,9 +8,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductComponent implements OnInit {
 
-  constructor() { }
+  private productId: number;
+  constructor(private routeInfo: ActivatedRoute) { }
 
   ngOnInit() {
+    this.productId = this.routeInfo.snapshot.queryParams["id"];
   }
 
 }
