53e2270fbc4d33ba50d9e97716b057e85aaee192
JeffreyLiuBit <17771770679@163.com>
2019-06-16 20:38:41 +0800

    辅助路由Ok

--- router/src/app/app-routing.module.ts
+++ router/src/app/app-routing.module.ts
@@ -5,9 +5,11 @@ import {ProductComponent} from "./product/product.component";
 import {Code404Component} from "./code404/code404.component";
 import { ProductDescComponent } from './product-desc/product-desc.component';
 import { SellerInfoComponent } from './seller-info/seller-info.component';
+import { ChatComponent } from './chat/chat.component';
 
 const routes: Routes = [
   {path: '' , redirectTo: '/home',pathMatch:'full'},
+  {path: 'chat' , component: ChatComponent,outlet:'aux'},
   {path: 'home' , component: HomeComponent},
   {path: 'product' , component: ProductComponent},
   {path: 'product/:id' , component: ProductComponent,children:[
